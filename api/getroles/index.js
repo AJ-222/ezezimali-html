5d2804929690032661f0a98c32fb2261c9624b99
AJ-222 <2538862@students.wits.ac.za>
2024-05-01 08:55:08 +0200

    Another change

--- api/getroles/index.js
+++ api/getroles/index.js
@@ -1,8 +1,6 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     const roles = [];
-    
-    // Code to check for user's current AAD roles and add to the 'roles' array
 
     context.res = {
         status: 200,
